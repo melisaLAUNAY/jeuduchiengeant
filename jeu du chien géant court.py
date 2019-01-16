97d0cdfcb7830d85e1e7c23a2a70c3037f435743
melisaLAUNAY <46750116+melisaLAUNAY@users.noreply.github.com>
2019-01-16 14:23:44 +0100

    Add files via upload

---
+++ jeu du chien géant court.py
@@ -0,0 +1,21 @@
+print("Tu te trouves dans le ventre d'un chien géant")
+print("Tu dois choisir par quel trou tu dois t'enfuir")
+choixJoueur=input("narines,anus ou oreilles?")
+if choixJoueur=="narines":
+    print("haha tu es rentré en collision avec des crottes de nez")
+    print("Game over, loser!")
+elif choixJoueur=="anus":
+    print("ça sent mauvais,la lumière est proche!")
+    print("Bravo,tu as réussi,maintenant va prendre une douche!")
+elif choixJoueur=="oreilles":
+          print("Elles sont bouchées!Impossible de passer par là!")
+          print("Game over,loser!")
+else:
+          print("Gros con, tu peux pas passer par là!")
+          print("Tu vas te faire digéré par le chien génat!")
+print("Il y a un bonus,répond à la question suivante:")
+print("Qu'est-ce qui sent mauvais et est marron?")
+print("Réessaye le jeu")
+while choixJoueur=="caca":
+    print("va chier!")
+
